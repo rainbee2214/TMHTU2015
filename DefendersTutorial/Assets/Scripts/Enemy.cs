a9b3202ada06689f1c598141ebfadb33d2130564
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-10-16 20:38:53 -0300

    Enemy movement and tracking.

--- DefendersTutorial/Assets/Scripts/Enemy.cs
+++ DefendersTutorial/Assets/Scripts/Enemy.cs
@@ -19,11 +19,46 @@ public class Enemy : Ship, IHealth
     new void Awake()
     {
         base.Awake();
+        shipName = GetRandomName();
+        player = GameObject.FindObjectOfType<Player>().gameObject;
+        planet = GameObject.FindObjectOfType<Planet>().gameObject;
     }
 
     new void Update()
     {
         base.Update();
+        if (dead) return;
+
+        if (Vector3.Distance(player.transform.position, transform.position) > trackDistance)
+        {
+            //Going towards planet
+            transform.position = Vector3.MoveTowards(transform.position, planet.transform.position, speed * Time.deltaTime);
+            Vector3 v = planet.transform.position - transform.position;
+
+            if (planet.transform.position.y - transform.position.y < 0)
+            {
+                movement.EnemyRotate(-Mathf.Atan(v.x / v.y) * 180 / Mathf.PI - 180f);
+            }
+            else
+            {
+                movement.EnemyRotate(-Mathf.Atan(v.x / v.y) * 180 / Mathf.PI);
+            }
+        }
+        else
+        {
+            //Going towards player
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+            Vector3 v = player.transform.position - transform.position;
+            if (player.transform.position.y - transform.position.y < 0)
+            {
+                movement.EnemyRotate(-Mathf.Atan(v.x / v.y) * 180 / Mathf.PI - 180f);
+            }
+            else
+            {
+                movement.EnemyRotate(-Mathf.Atan(v.x / v.y) * 180 / Mathf.PI);
+            }
+        }
+        //if (Vector3.Distance(player.transform.position, transform.position) > shootDistance)
     }
 
     string GetRandomName()
